d877952d4a8948040396f17de6f1c5cfdc468e44
A.Gökhan SATMAN <abgsatman@hotmail.com>
2025-04-03 21:09:23 +0300

    Some paths are edited.

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/abgsatman/minigoapi/internal/handler"
+	"github.com/abgsatman/minigoapi/internal/userapi"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hc", handler.HcHandler).Methods("GET")
+	r.HandleFunc("/hc", userapi.HcHandler).Methods("GET")
 
 	fmt.Println("Server is running at localhost:8080")
+	http.ListenAndServe(":8080", r)
+
 }
